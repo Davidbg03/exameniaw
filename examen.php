e044de91953a88e40a8e6da703d9aebb37201f77
David <dbg@dbg.es>
2024-10-31 09:05:50 +0000

    Segundo commit

--- examen.php
+++ examen.php
@@ -1,3 +1,4 @@
-<?php echo 'David Bejarano - 31/10/2024'?>
-<?php echo 'Un repositoria es un lugar de almacenamiento de datos'?>
+<h1><?php echo 'David Bejarano'?></h1>
+<h2<?php echo '31/10/2024'?></h2>
+<h3><?php echo 'Un repositoria es un lugar de almacenamiento de datos'?></h3>
 
